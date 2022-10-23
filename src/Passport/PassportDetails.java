8bb9545b7962a8c9ff70a4c36ef570f78d69a267
Alex <alexsa-8@mail.ru>
2022-10-23 20:21:34 +0300

    паспорта. не получается из класса  PassportDetails сравнить номера паспартов. В строке 23 equals выделяется красным. Два метода перенёс в Passport. Со скрипом всё идёт. Подскажите в чём причина?

---
+++ src/Passport/PassportDetails.java
@@ -0,0 +1,36 @@
+package Passport;
+
+//import java.util.ArrayList;
+//import java.util.List;
+
+public class PassportDetails {
+//    private final List<Passport> passportList = new ArrayList<>();
+//
+//    public void savePassportData(Passport passport) throws PassportException {
+//        Passport passport1 = lookingPassportNumber(passport.getNumberPassport());
+//        if (passport1 == null) {
+//            passportList.add(passport);
+//        } else {
+//            passport1.setLastName(passport.getLastName());
+//            passport1.setName(passport.getName());
+//            passport1.setPatronymic(passport.getPatronymic());
+//            passport1.setDateOfBirth(passport.getDateOfBirth());
+//        }
+//    }
+//
+//    public Passport lookingPassportNumber(int number) {
+//        for (Passport passport : passportList) {
+//            if (passport.getNumberPassport().equals(number)) {
+//                return passport;
+//            }
+//        }
+//        return null;
+//    }
+//
+//    @Override
+//    public String toString() {
+//        return "PassportDetails{" +
+//                "passportList=" + passportList +
+//                '}';
+//    }
+}
