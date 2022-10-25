73023918d8ca40786f51d61e948405fc97964018
Alex <alexsa-8@mail.ru>
2022-10-25 21:59:01 +0300

    Коллекции 3 урок. Первые 3 задания.

---
+++ src/hw_6_2/Collection.java
@@ -0,0 +1,86 @@
+package hw_6_2;
+
+import java.util.*;
+
+public class Collection {
+    Map<String, List<Integer>> collection= new HashMap<>();
+    List<Integer> integerList= new ArrayList<>();
+    String element;
+
+    Integer sum;
+
+    public Collection(String element) {
+        this.element = element;
+    }
+
+    Random random = new Random();
+
+    private void randoms() {
+        int randomNumber1 = random.nextInt(1000);
+        int randomNumber2 = random.nextInt(1000);
+        int randomNumber3 = random.nextInt(1000);
+        integerList.add(randomNumber1);
+        integerList.add(randomNumber2);
+        integerList.add(randomNumber3);
+    }
+
+    public void Transformation(String collection) {
+        this.collection.put(collection, integerList);
+        element = collection;
+    }
+
+    private void sumOfRandomNumbers() {
+        sum = integerList.get(0) + integerList.get(1) + integerList.get(2);
+    }
+    public void collectionList(){
+        Map<String, Integer> collectionList= new HashMap<>();
+        collectionList.put(element, sum);
+        System.out.println(collectionList);
+    }
+
+    public String getCollection() {
+        StringBuilder builder=new StringBuilder();
+        for (Map.Entry<String, List<Integer>> entry : collection.entrySet()) {
+            builder.append(entry.getKey()).append(" --> ");
+            for (Integer collection : entry.getValue()){
+                builder.append(collection).append(",");
+            }
+
+        }
+        return builder.toString();
+    }
+
+    public List<Integer> getIntegerList() {
+        return integerList;
+    }
+
+    public String getElement() {
+        return element;
+    }
+
+    public Integer getSum() {
+        return sum;
+    }
+
+    public Random getRandom() {
+        return random;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Collection that = (Collection) o;
+        return Objects.equals(collection, that.collection) && Objects.equals(integerList, that.integerList) && Objects.equals(element, that.element) && Objects.equals(sum, that.sum) && Objects.equals(random, that.random);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(collection, integerList, element, sum, random);
+    }
+
+    @Override
+    public String toString() {
+        return "Collection " + getCollection();
+    }
+}
---
+++ src/hw_6_2/Main.java
@@ -0,0 +1,28 @@
+package hw_6_2;
+
+import java.util.*;
+
+public class Main {
+    private static Object collectionList;
+
+    public static void main(String[] args) {
+        String element="Элемент";
+        Collection element1=new Collection("Элемент");
+        Map<String, List<Integer>> collection= new HashMap<>();
+        List<Integer> integerList = new ArrayList<>();
+
+        Random random = new Random();
+        int randomNumber1 = random.nextInt(1000);
+        int randomNumber2 = random.nextInt(1000);
+        int randomNumber3 = random.nextInt(1000);
+        integerList.add(randomNumber1);
+        integerList.add(randomNumber2);
+        integerList.add(randomNumber3);
+        collection.put("элемент", List.of(randomNumber1,randomNumber2,randomNumber3));
+        System.out.println(collection);
+        int sum = integerList.get(0) + integerList.get(1) + integerList.get(2);
+        Map<String, Integer> collectionList= new HashMap<>();
+        collectionList.put(element, sum);
+        System.out.println(collectionList);
+    }
+}
